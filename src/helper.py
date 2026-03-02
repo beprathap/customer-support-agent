c72a1331b65fb41c59f13922bcb4afe6c97471f5
beprathap <prathapboddumail@gmail.com>
2026-03-02 08:13:08 -0800

    complete helper

--- src/helper.py
+++ src/helper.py
@@ -91,3 +91,16 @@ def call_llm(query: str, responses: List[str]) -> str:
     """
     #assuming your KEY is saved in your environment variable as described in the Readme
     client = OpenAI(api_key=API_KEY)
+
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant and help answer customer's query and request. Answer on the basis of provided context only."},
+        {"role": "user", "content": f'''On the basis of the input customer query determine or suggest the following things about the input query: {query}:
+                                          1. Urgency of the query based on the input query on a scale of 1-5 where 1 is least urgent and 5 is most urgent. Only tell me the number.
+                                          2. Categorize the input query into sales, product, operations etc. Only tell me the category.
+                                          3. Generate 1 best humble response to the input query which is similar to examples in the python list: {responses} from the internal database and is helpful to the customer.
+                                          If the input query form customer is not clear then ask a follow up question.                  
+                                      '''}
+    ]
+    response = client.chat.completions.create(model="gpt-4o", messages=messages, temperature=0)
+    
+    return response.choices[0].message.content
